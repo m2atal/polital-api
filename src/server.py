7d9d8be9a68a7a410d222296494a2dda6bbd0b2b
Valentin Pelloin <valentinpelloin@gmail.com>
2019-11-21 18:47:16 +0100

    now using a proper stopword list

--- src/server.py
+++ src/server.py
@@ -1,12 +1,8 @@
 import pdftotext
-import spacy
-from flask import Flask, request, redirect, jsonify
+from stop_words import get_stop_words
+from flask import Flask, request, jsonify
 from multi_rake import Rake
 from gensim.summarization.summarizer import summarize
-#from gensim.summarization.keywords import keywords
-from summa import keywords
-#from summa import summarizer
-
 
 ALLOWED_EXTENSIONS = set(['pdf'])
 KEYWORDS_COUNT     = 15
@@ -15,8 +11,7 @@ SUMMARY_COUNT      = 500
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
-nlp  = spacy.load('fr_core_news_sm')
-sw   = spacy.lang.fr.STOP_WORDS
+sw   = set(get_stop_words('fr'))
 rake = Rake(language_code='fr', stopwords=sw, min_freq=2)
 
 def allowed_file(filename):
@@ -38,12 +33,8 @@ def upload_file():
         pdf = pdftotext.PDF(file)
         text = "\n".join(pdf)
         text = text.replace('\n', ' ')
-        #keywords_text = keywords(text, split=True, scores=True)
         keywords_text = [kw[0] for kw in rake.apply(text.lower())[:KEYWORDS_COUNT]]
         summary_text  = ". ".join(summarize(text, word_count=SUMMARY_COUNT, split=True))
-        #keywords_text = keywords.keywords(text, language='french', words=15, split=True)
-        #summary_text  = summarizer.summarize(text, language='french', split=True, words=SUMMARY_COUNT)
-
 
         resp = jsonify({
             'keywords': keywords_text,
